193aab898e20e367667ebf53fdaeec9802053b3a
minionsrahat <rahatuddin786@gmail.com>
2022-04-29 00:47:22 +0600

    Project Files Included

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import ExpenseForm from './Components/ExpenseForm/ExpenseForm';
+import Expenses from './Components/Expenses/Expenses';
+import uuid from 'react-uuid'
+import { useEffect, useState } from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar></Navbar>
+    <Routes>
+    <Route path='/' element={<PrivateRoute><Home></Home></PrivateRoute>}></Route>
+      <Route path='/home' element={
+      <PrivateRoute><Home></Home></PrivateRoute>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
+    </Routes>
+
+    </>
   );
 }
 
